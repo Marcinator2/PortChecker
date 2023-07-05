4df55e9a7c9f6e1f3c1d13708b4d7842de83f9a8
Marcinator2 <marcinator_2@gmx.de>
2023-07-05 21:43:58 +0200

    edited IPs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Net;
+//using System.Net;
 using System.Net.Sockets;
 using System.IO;
 using System.Net.NetworkInformation;
@@ -11,7 +11,7 @@ namespace PortChecker
     {
         static void Main(string[] args)
         {
-            int[] portsToCheck = { 21, 22, 8083, 8323, 11001 };
+            int[] portsToCheck = { 21, 22, 8083, 8323, 10001 };
             string filePath = "iplist.csv";
             string resultFile = "ipportresult.csv";
 
